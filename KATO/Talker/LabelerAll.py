5dd6d9a39e5dd00bc3ac506687d2d5a8e40d1bac
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-08-19 23:40:21 +0700

    token limitation

--- KATO/Talker/LabelerAll.py
+++ KATO/Talker/LabelerAll.py
@@ -6,20 +6,43 @@ class LabelerAll(LabelerBase):
     全ての書き起こしデータをそのまま教師データとして保存するクラス。
     '''
     
-    def __init__(self) -> None:
+    def __init__(
+        self, 
+        max_text_length = 1000, #一つの文字列が最大何文字格納されるか 
+        proceedings = 3     #文字列数増加時に、前回のリストの何テキスト文を重複して含めるか
+    ) -> None:
         super().__init__(pathlib.Path(__file__).parent)
         self.data_name = "teacher_data_all"
+        self.max_text_length = max_text_length
+        self.proceedings = proceedings
     
     def process_transcription(self, transcriptions):
-        new_list = []
-        previous = ""
+        text_list_list = []
         #連続重複を除外する
+        current_length = 0
+        text_list_current = [""]
         for text in transcriptions:
-            if text != previous:
-                new_list.append(text)
-                previous = text
+            if text != text_list_current[-1]:
+                length = len(text)
+                if current_length + length <= self.max_text_length:
+                    text_list_current.append(text)
+                    current_length += length
+                else:
+                    text_list_list.append(text_list_current)
+                    text_list_current = text_list_current[-self.proceedings:]
+                    text_list_current.append(text)
+                    current_length = 0
+                    for _text in text_list_current:
+                        current_length += len(_text)
+                        
+        text_list_list.append(text_list_current)
         
-        return [" ".join(new_list)]
+        output_list = []
+        
+        for text_list in text_list_list:
+            output_list.append(" ".join(text_list))
+        
+        return output_list
     
     def save_data(self, df_teacher_data, year):
         df_teacher_data.to_csv(self.cwd / "teacher_data_all" / (self.data_name + "_" + str(year) + ".csv"), index=True)
